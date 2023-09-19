830e118ab8f86da994a73ae66b7619f73ca59edd
deepanshubagde <93531343+deepanshubagde@users.noreply.github.com>
2023-09-19 11:24:11 +0000

    Pending changes exported from your codespace

---
+++ datatable/test.php
@@ -0,0 +1,10 @@
+<?php 
+$data = [ "data" => [
+    ["name" => "pratik", "age" => 24],
+    ["name" => "shivam" , "age" => 35]
+
+]];
+//print_r($data);
+
+echo json_encode($data);
+?>
---
+++ php/index.php
@@ -0,0 +1,5 @@
+<?php
+
+$x = 5;
+$sqr = $x * $x;
+echo "Square of $x is $sqr";
